f216c08cd55abc7645ac0a8577fb09a8243a2791
Michael Pories <pories@hotmail.com>
2019-01-15 06:56:42 -0500

    Added main menu
    Filled with options.

---
+++ MainMenu/src/Execute.java
@@ -0,0 +1,10 @@
+public class Execute {
+
+	public static void main(String[] args) {
+		Boolean displayMenu = true;
+        while (displayMenu)
+        {
+            displayMenu = MainMenu.userMainMenu();           
+        }
+	}
+}
---
+++ MainMenu/src/Null.java
@@ -0,0 +1,8 @@
+
+public class Null {
+	public static void Object()
+	{
+		System.out.println("Not a listed option.");
+	}
+	
+}
